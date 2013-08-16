e28c6d26c8fde77da9105f205c2e87bebbcd6584
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-08-16 13:47:22 +0200

    Fixed some bugs during parsing and translation

--- parser.py
+++ parser.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--scheme', default="http", help="http or https", choices=['http', 'https'])
 parser.add_argument('--upstream', help="Ngnix's upstream", required=True)
 parser.add_argument('--csv', help="Input file", required=True)
+
 parser.add_argument('--output', help="Output file", required=True)
 parser.add_argument('--base_old_url', help="Base url for the old urls", required=True)
 parser.add_argument('--base_new_url', help="Base url for the new urls", required=True)
@@ -14,7 +15,6 @@ parser.add_argument('--language', action="append", help="Languags for localizing
 
 args = parser.parse_args()
 
-print(args)
 def printLocation(scheme, host, language, oldUrl, newUrl, f):
         if language:
             print("location = /" + language + oldUrl + "{", file=f)
@@ -25,10 +25,11 @@ def printLocation(scheme, host, language, oldUrl, newUrl, f):
         print(os.linesep, file=f)
 
 
-def parseCSV(filename):
+def parseCSV(filename,  oldHostUrl, newHostUrl):
     urls = []
     urls_obsolete = []
     with open(filename, "rb") as f:
+	print(f)
         reader = csv.reader(f)
         rows = [r for  r in reader if (r[0] and not (r[0] == "Next page"))  ][1:]
         for row in rows:
@@ -39,6 +40,7 @@ def parseCSV(filename):
                 urls_obsolete.append((oldUrl, newUrl))
             else:
                 urls.append((oldUrl, newUrl))
+    return (urls, urls_obsolete)
 def translateToNgnix(outputFileName, scheme, host, urls, languages):    
     with open(outputFileName, "w") as f:
         for old, new in urls:
@@ -48,5 +50,14 @@ def translateToNgnix(outputFileName, scheme, host, urls, languages):
             else:
                 printLocation(scheme, host, None, old, new, f)
 
+scheme = args.scheme
+upstream = args.upstream
+csvFilename = args.csv
+output = args.output
+baseOldUrl = args.base_old_url
+baseNewUrl = args.base_new_url
+language = args.language
 
-
+urls = parseCSV(csvFilename, baseOldUrl, baseNewUrl)
+translateToNgnix(output, scheme, upstream, urls[0], language)
+translateToNgnix(output+"_obsolete", scheme, upstream, urls[1], language)
